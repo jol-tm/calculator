7119d0b3ddc39b8cc7cff78f9b673554f7806733
jol-tm <joaolucastmagalhaes@gmail.com>
2024-06-25 21:43:00 -0300

    Added more buttons and adjusted layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-    $('button').click(calc);
+    $('button').on('click', calc);
+    $('#display').on('keypress', function(k) {
+        if (k.code == 'Enter'){
+            calc(k);
+        }
+    });
 });
 
 let input = '';
 
-function calc() {
+function calc(k) {
     if ($(this).attr('value') == 'c') {
         $('#display').val('');
         input = '';
-    } else if ($(this).attr('value') == '=') {
-        $('#display').val(eval($('#display').val().replace('X', 'x').replace('x', '*').replace('÷', '/')));
+    } else if ($(this).attr('value') == '=' || k.code == 'Enter') {
+        $('#display').val(eval($('#display').val().replace('X', 'x').replace('x', '*').replace('÷', '/').replace(',', '.')));
         input = '';
     } else {
         input += $(this).attr('value');
