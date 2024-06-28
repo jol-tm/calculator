837d7ec282fc9cff006ca3d0f177dd482b85c91e
jol-tm <joaolucastmagalhaes@gmail.com>
2024-06-28 17:04:53 -0300

    Fixed a bug when entering values ​​after pressing equals button with nothing to calculate and added the possibility to use '^' in potentiation

--- scripts/main.js
+++ scripts/main.js
@@ -13,13 +13,13 @@ $(document).ready(function() {
 let input = '';
 
 function calc(k) {
-    if ($(this).attr('value') == 'c') {
+    input = $('#display').val();
+    if ($(this).attr('value') == '=' || k.code == 'Enter' && input != '') {
+        input = eval(input.toLowerCase().replace('x', '*').replace('÷', '/').replace('^', '**').replace(',', '.'));
+        $('#display').css('boxShadow', '0 0 2rem rgb(0, 190, 255)');
+    } else if ($(this).attr('value') == 'c') {
         input = '';
         $('#display').css('boxShadow', 'none');
-    } else if ($(this).attr('value') == '=' || k.code == 'Enter') {
-        input = $('#display').val();
-        input = eval(input.toLowerCase().replace('x', '*').replace('÷', '/').replace(',', '.'));
-        $('#display').css('boxShadow', '0 0 2rem rgb(0, 190, 255)');
     } else {
         input += $(this).attr('value');
         $('#display').css('boxShadow', 'none');
